5eb64642c8cd952814286fbd6562bcdb12c54e6c
sofia <ssarmiento088@gmail.com>
2024-09-19 19:54:49 -0500

    commit 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <nav className="bg-white p-4 font-semibold mflex justify-center">
-      <div className="container mx-auto flex justify-between items-center text-pretty">
-        <div className="flex space-x-2 font-bold text-xl items-center mr-14">
+    <nav className="bg-white p-4 font-semibold flex justify-center w-full">
+      <div className="container flex justify-between items-center text-pretty w-full">
+        <div className="flex space-x-2 font-bold text-xl items-center ml-5 mr-5">
           <div>
             <HiCode></HiCode>
           </div>
@@ -14,11 +14,11 @@ const Header = () => {
             Softcode
           </div>
         </div>
-        <ul className="flex space-x-6 font-semibold">
+        <ul className="flex font-semibold text-black pl-3 pr-3 justify-center">
           <li>
             <Link
               to="/"
-              className="hover:text-blue-800 transition-colors"
+              className="hover:text-blue-800 transition-colors text-black pl-3 pr-3"
             >
               Home
             </Link>
@@ -26,7 +26,7 @@ const Header = () => {
           <li>
             <Link
               to="/aboutus"
-              className="hover:text-blue-800 transition-colors"
+              className="hover:text-blue-800 transition-colors text-black pl-3 pr-3"
             >
               Sobre nosotros
             </Link>
@@ -34,7 +34,7 @@ const Header = () => {
           <li>
             <Link
               to="/courses"
-              className="hover:text-blue-800 transition-colors"
+              className="hover:text-blue-800 transition-colors text-black pl-3 pr-3"
             >
               Cursos
             </Link>
@@ -42,17 +42,17 @@ const Header = () => {
           <li>
             <Link
               to="/services"
-              className="hover:text-blue-800 transition-colors"
+              className="hover:text-blue-800 transition-colors text-black pl-3 pr-3"
             >
               Servicios
             </Link>
           </li>
         </ul>
-        <div className='flex space-x-6 font-semibold'>
-            <button className='text-yellow-300 hover:text-black'>
+        <div className='flex space-x-6 font-semibold bg-white justify-end'>
+            <button className='text-yellow-300 hover:text-black bg-white hover:border-white'>
                 Iniciar Sesión
             </button>
-            <button className='text-blue-800 border border-blue-800 p-2 rounded-md hover:text-black hover:border-black'>
+            <button className='text-blue-800 border border-blue-800 p-2 rounded-md hover:text-black hover:border-black bg-white'>
                 Registrarse
             </button>
         </div>
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+//import React from 'react';
 import { Cards } from '../components/Cards';
 
 
@@ -11,16 +11,13 @@ import { SiHtmlacademy } from "react-icons/si";
 
 const Services = () => {
   return (
-    <div className="block justify-center items-center min-h-screen">
-        <div className="p-16"/>
-        <div className='flex justify-between max-w-full mx-10'>
-            <h1 className="pl-32 pb-8 text-black align-left max-w-full mx-auto">
-                Nuestros Servicios
-            </h1>
-            <h1 className="ml-0 pt-8 pb-8 text-black text-sm text-justify max-w-xs mx-auto">
-            Ofrecemos cursos de programación accesibles y prácticos, diseñados para impulsar tu carrera en el mundo digital. ¡Aprende y crece con nosotros!
-            </h1>
-        </div>
+    <div className="block justify-center items-center w-full min-h-screen bg-white py-10">
+      <h1 className="text-3xl font-bold text-gray-800 text-center mb-5">
+        Nuestros servicios
+      </h1>
+      <h4 className="text-md text-gray-500 text-center mb-8">
+         Ofrecemos cursos de programación accesibles y prácticos, diseñados para impulsar tu carrera en el mundo digital. ¡Aprende y crece con nosotros!
+      </h4>
 
       <div className="grid gap-4 grid-cols-3 grid-rows-2 max-w-screen-lg w-full mx-auto">
         <Cards icon={FaPencilAlt} tittle="Cursos Personalizados" text="Aprende a tu ritmo con planes adaptados a tus necesidades." color="red" />
